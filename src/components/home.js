47e76dc52ffb7dd3b533868274333c8fdda28886
sandali <pldsandali@gmail.com>
2024-01-28 16:59:25 +0530

    added the social media links

--- src/components/home.js
+++ src/components/home.js
@@ -23,10 +23,21 @@ export default function Home() {
                 <div className="min-h-full ... h-dvh bg-cover bg-center bg-top lg:w-1/2 bg-[url('components/images/MyPic4.jpg')]" ></div>
                 <div className='lg:w-1/2 place-self-center '>
                     <div className='place-self-center'>
-                    <p className='text-6xl font-extrabold m-8'>SANDALI LIYANAGE</p>
+                    <p className='text-6xl font-extrabold '>SANDALI LIYANAGE</p>
                     <br></br>
                     <p className='text-2xl'>I'm a Computer Science & Engineering Undergraduate</p>
                     <p className='text-2xl'>at University of Moratuwa, Sri Lanka</p>
+                    <div >
+                        <ul className='flex flex-row justify-center m-8'>
+                            <li><a href="https://www.facebook.com/sandalikLiyanage"><img className= "m-5" src="linkedin.png"></img></a></li>
+                            <li><a href="https://www.facebook.com/sandalikLiyanage"><img className= "m-5" src="facebook2.png"></img></a></li>
+                            <li><a href="https://www.facebook.com/sandalikLiyanage"><img className= "m-5" src="instagram2.png"></img></a></li>
+                            <li><a href="https://www.facebook.com/sandalikLiyanage"><img className= "m-5" src="github.png"></img></a></li>
+                            <li><a href="https://www.facebook.com/sandalikLiyanage"><img className= "m-5" src="twitter.png"></img></a></li>
+
+
+                        </ul>
+                    </div>
                     </div>
                 </div>
             </div>
