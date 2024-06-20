18621035b5e9d0fe5f51d9c819ec738dc304066a
sandali <pldsandali@gmail.com>
2024-06-21 00:30:49 +0530

    added alt tags

--- src/components/contacts.js
+++ src/components/contacts.js
@@ -16,11 +16,11 @@ export default function Contacts() {
           </div>
           <div className='lg:w-1/2 place-self-center'>
             	<ul className='flex flex-row justify-center m-8' >
-                <li><a href="www.linkedin.com/in/sandaliliyanage-cse"><img className= "m-5" src={process.env.PUBLIC_URL + "/linkedin.png" }></img></a></li>
-                <li><a href="https://www.facebook.com/sandalikLiyanage"><img className= "m-5" src={process.env.PUBLIC_URL + "/facebook2.png"}></img></a></li>
-                <li><a href="https://github.com/SandaliLiyanage"><img className= "m-5" src={process.env.PUBLIC_URL + "/github.png"}></img></a></li>
-                <li><a href="https://www.facebook.com/sandalikLiyanage"><img className= "m-5" src={process.env.PUBLIC_URL + "/instagram2.png"}></img></a></li>
-                <li><a href="https://www.facebook.com/sandalikLiyanage"><img className= "m-5" src={process.env.PUBLIC_URL + "/twitter.png"}></img></a></li>
+              <li><a href="www.linkedin.com/in/sandaliliyanage-cse"><img className= "m-5" src={process.env.PUBLIC_URL + "/linkedin.png" } alt = "linkedin icon"></img></a></li>
+                            <li><a href="https://www.facebook.com/sandalikLiyanage"><img className= "m-5" src={process.env.PUBLIC_URL + "/facebook2.png"} alt = "facebook icon"></img></a></li>
+                            <li><a href="https://github.com/SandaliLiyanage"><img className= "m-5" src={process.env.PUBLIC_URL + "/github.png"} alt = "github icon"></img></a></li>
+                            <li><a href="https://www.facebook.com/sandalikLiyanage"><img className= "m-5" src={process.env.PUBLIC_URL + "/instagram2.png"}  alt = "instagram icon"></img></a></li>
+                            <li><a href="https://www.facebook.com/sandalikLiyanage"><img className= "m-5" src={process.env.PUBLIC_URL + "/twitter.png"} alt = "twitter icon"></img></a></li>
 
 
             </ul>
--- src/components/home.js
+++ src/components/home.js
@@ -13,11 +13,13 @@ export default function Home() {
   useEffect(()=> {
     ScrollReveal().reveal('.header-text',{  duration:2500, distance:'500px', origin:'bottom'});  
   })
+
+  
   return (
     <section id="home">
         <div id ="header">
             <div className='flex ... flex-col lg:flex-row'>
-                <div className="image min-h-full ... h-dvh lg:w-1/2 mt-16 bg-contain bg-center bg-top bg-no-repeat bg-[url('components/images/MyPic4.jpg')]" ></div>
+                <div className="image min-h-full ... h-dvh lg:w-1/2 mt-16 bg-contain bg-center bg-top bg-no-repeat bg-[url('components/images/MyPic4.jpg')]" alt = "bg" ></div>
                 <div className='lg:w-1/2 lg:place-self-center header-text m-6 lg:mr-16'>
                     <div className='place-self-center'>
                     <p className='text-3xl sm:text-5xl md:text-6xl font-extrabold '>SANDALI LIYANAGE</p>
@@ -26,11 +28,11 @@ export default function Home() {
                     <p className='text-2xl'>at University of Moratuwa, Sri Lanka</p>
                     <div>
                         <ul className='flex flex-row justify-center' >
-                            <li><a href="www.linkedin.com/in/sandaliliyanage-cse"><img className= "m-5" src={process.env.PUBLIC_URL + "/linkedin.png" }></img></a></li>
-                            <li><a href="https://www.facebook.com/sandalikLiyanage"><img className= "m-5" src={process.env.PUBLIC_URL + "/facebook2.png"}></img></a></li>
-                            <li><a href="https://github.com/SandaliLiyanage"><img className= "m-5" src={process.env.PUBLIC_URL + "/github.png"}></img></a></li>
-                            <li><a href="https://www.facebook.com/sandalikLiyanage"><img className= "m-5" src={process.env.PUBLIC_URL + "/instagram2.png"}></img></a></li>
-                            <li><a href="https://www.facebook.com/sandalikLiyanage"><img className= "m-5" src={process.env.PUBLIC_URL + "/twitter.png"}></img></a></li>
+                            <li><a href="www.linkedin.com/in/sandaliliyanage-cse"><img className= "m-5" src={process.env.PUBLIC_URL + "/linkedin.png" } alt = "linkedin icon"></img></a></li>
+                            <li><a href="https://www.facebook.com/sandalikLiyanage"><img className= "m-5" src={process.env.PUBLIC_URL + "/facebook2.png"} alt = "facebook icon"></img></a></li>
+                            <li><a href="https://github.com/SandaliLiyanage"><img className= "m-5" src={process.env.PUBLIC_URL + "/github.png"} alt = "github icon"></img></a></li>
+                            <li><a href="https://www.facebook.com/sandalikLiyanage"><img className= "m-5" src={process.env.PUBLIC_URL + "/instagram2.png"}  alt = "instagram icon"></img></a></li>
+                            <li><a href="https://www.facebook.com/sandalikLiyanage"><img className= "m-5" src={process.env.PUBLIC_URL + "/twitter.png"} alt = "twitter icon"></img></a></li>
 
 
                         </ul>
